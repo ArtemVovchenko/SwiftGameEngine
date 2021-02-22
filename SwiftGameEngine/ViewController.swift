5df1bfbf32900df3ef3b3f5dfd29f6bc8dd0e4a0
Artem Vovchenko <artem.vovchenko@nure.ua>
2021-02-23 00:07:57 +0200

    Initial Commit

---
+++ SwiftGameEngine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftGameEngine
+//
+//  Created by Artem Vovchenko on 23.02.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
